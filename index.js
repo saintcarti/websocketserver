5fb18de86f7cb9d8fd0794b0e83f14e4905017b3
saintcarti <xa.liberona@duocuc.cl>
2024-11-23 03:34:10 -0300

    mensaje

--- index.js
+++ index.js
@@ -25,6 +25,26 @@ async function obtenerEventosDesdeApi() {
   }
 }
 
+async function obtenerUsuariosDesdeApi(){
+    try{
+        const respuesta = await axios.get('https://repojson-zdrg.onrender.com/usuarios');
+        return respuesta.data;
+    }catch(error){
+        console.error('Error al obtener los usuarios:',error);
+        return [];
+    }
+}
+
+async function obtenerParticipacionDesdeApi(){
+    try{
+        const respuesta = await axios.get('https://repojson-zdrg.onrender.com/Participacion');
+        return respuesta.data;
+    }catch(error){
+        console.error('Error al obtener la participacion:',error);
+        return [];
+    }
+}
+
 // Cuando un cliente se conecta
 io.on('connection', (socket) => {
   console.log('Un cliente se ha conectado');
@@ -34,12 +54,22 @@ io.on('connection', (socket) => {
     socket.emit('evento-actualizado', eventos);
   });
 
+  obtenerUsuariosDesdeApi().then((usuarios)=>{
+    socket.emit('usuario-actualizado',usuarios);
+  });
+
   // Puedes emitir datos cuando los eventos cambian o son actualizados
   setInterval(() => {
     obtenerEventosDesdeApi().then((eventos) => {
       io.emit('evento-actualizado', eventos); // Emitir evento actualizado a todos los clientes
     });
   }, 3000); // Actualizar cada 5 segundos (por ejemplo)
+
+  setInterval(()=>{
+    obtenerUsuariosDesdeApi().then((usuarios)=>{
+        io.emit('usuario-actualizado',usuarios);
+    });
+  },3000);
 
   socket.on('disconnect', () => {
     console.log('Un cliente se ha desconectado');
