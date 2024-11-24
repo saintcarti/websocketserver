25fe4eeb53dda93efa3333148d8a9c0ea459fd9f
saintcarti <xa.liberona@duocuc.cl>
2024-11-23 21:43:34 -0300

    mensaje

--- index.js
+++ index.js
@@ -68,19 +68,19 @@ io.on('connection', (socket) => {
     obtenerEventosDesdeApi().then((eventos) => {
       io.emit('evento-actualizado', eventos); // Emitir evento actualizado a todos los clientes
     });
-  }, 5000); // Actualizar cada 5 segundos (por ejemplo)
+  }, 50000); // Actualizar cada 5 segundos (por ejemplo)
 
   setInterval(()=>{
     obtenerUsuariosDesdeApi().then((usuarios)=>{
         io.emit('usuario-actualizado',usuarios);
     });
-  },5000);
+  },50000);
 
   setInterval(()=>{
     obtenerParticipacionDesdeApi().then((participacion)=>{
         io.emit('participacion-actualizado',participacion);
     });
-  },5000)
+  },50000)
 
   socket.on('disconnect', () => {
     console.log('Un cliente se ha desconectado');
